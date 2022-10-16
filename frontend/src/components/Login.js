0ff76950bd1ece7d32d3ef2925f9fd8e76e36011
Shih-Hua <79493876+shihhu0a0@users.noreply.github.com>
2022-10-15 22:17:23 -0400

    Fix Login.js syntax error

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,24 +4,20 @@ import productDataService from '../service/productDataService';
 import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
-
-  // const options = [
-  //   { label: "a TA", value: "TA" },
-  //   { label: "a friend of Zheng Duan", value: "friend" }
-  // ];
-
   const {user, setUser} = CartState();
 
   const initialSignInState = {
     user_name: "",
     password: "",
   };
+
   const [signInState, setsignInState] = useState(initialSignInState)
 
   const handleInputChange = event => {
     const { name, value } = event.target;
     setsignInState({ ...signInState, [name]: value });
   };
+
   const navigate = useNavigate();
   const signIn = async () => {
     let flag = false;
@@ -35,43 +31,6 @@ const Login = () => {
       if(!flag) alert("Incorrect Password")
   }
 
-
-  return (
-    <div className="singInForm">
-      <div className='main_login'>
-        <form className="form1">
-          <br/>
-          <input id='user_name' required 
-            className="username" type="text" placeholder="Username" 
-            onChange={handleInputChange} name="user_name" value={signInState.use_name}/>
-          <input id='password' required 
-            className="password" type="password" placeholder="Password" 
-            onChange={handleInputChange} name="password" value={signInState.password}/>
-          <br/><br/><br/>
-          <a className="submit" onClick={signIn} align="center">
-            Sign in
-          </a>
-          <br/><br/>
-          <div className="forgot" align="center">
-            <a onClick={() => {navigate('/signup')}}>Don't have an account? Sign up!</a>
-          </div>
-          <div className="forgot" align="center">
-            <a onClick={() => {alert("So Sad   :))))) 哈哈哈哈哈")}}>Forgot Password? </a>
-          </div>
-        </form>
-      </div>
-
-  const initialUserState = {
-    name: "",
-    id: "",
-  };
-
-  const [user, setUser] = useState(initialUserState);
-
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setUser({ ...user, [name]: value });
-  };
   return (
     <div>
       <form class="px-4 py-3">
@@ -109,12 +68,7 @@ const Login = () => {
           </div>
          </div>
       </form>
-         {/* <input type="text" placeholder="Name.." onChange={(e) => {setName(e.target.value)}} />
-//         <input type="number" placeholder="Age.."  onChange={(e) => {setAge(e.target.value)}}/>
-//         <input type="text" placeholder="Username.."  onChange={(e) => {setUsername(e.target.value)}}/> */}
-         {/* <button onClick={createUser}>Create User</button> */}
     </div>
-    
   )
 }
 
